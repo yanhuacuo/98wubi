6b804bc13315554efdfc6175a244c7a0a733c4a0
yanhuacuo <906515418@163.com>
2019-02-01 13:16:38 +0800

    Update rime.sh

--- linux/rime.sh
+++ linux/rime.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-apt install opencc libopencc-dev libyaml-cpp-dev -y
+echo "安装Librime运行环境"
+
+sudo apt install opencc libopencc-dev libyaml-cpp-dev -y
 
 cd ../opencc/
 
 echo "清理原属 Windows 下的 OCD 文件"
 
-rm -rf ~/*.ocd
+rm -rf ./*.ocd
 
 echo "清理完毕，开始制作 Linux 专属 OCD 文件"
 
@@ -18,52 +20,69 @@ opencc_dict -i STCharacters.txt -o STCharacters.ocd -f text -t ocd
 
 opencc_dict -i STPhrases.txt -o STPhrases.ocd -f text -t ocd
 
-chmod 777 ./*
+sudo chmod 777 ./*
+
+echo "成功制作 OCD 文件，即将准备安放配置文件"
 
 cd ../
 
-rm -rf ~/.config/fcitx/rime
+echo "清理目标文件夹"
 
-rm -rf ~/.config/ibus/rime
+rm -rf ~/.config/ibus/rime/build
+rm -rf ~/.config/fcitx/rime/build
+rm -rf ~/.config/fcitx/skin
+rm -rf ~/.config/fcitx/rime/*
+rm -rf ~/.config/ibus/rime/*
 
-rm -rf /usr/share/rime-data/*
+mkdir ~/.config/ibus/rime/build
+mkdir ~/.config/fcitx/rime/build
+mkdir ~/.config/fcitx/skin
 
-cp -rf ./*.yaml /usr/share/rime-data
-cp -rf ./opencc /usr/share/rime-data
+sudo rm -rf /usr/share/rime-data/*
+
+echo "准备添加fcitx皮肤文件"
+
+cp -rf ./98wb-B ~/.config/fcitx/skin
+
+echo "添加fcitx皮肤文件成功"
+
+echo "开始拷贝98五笔输入法文件"
+
+sudo cp -rf ./*.yaml /usr/share/rime-data
+sudo cp -rf ./opencc /usr/share/rime-data
+
 cp -rf ./*.yaml ~/.config/ibus/rime
 cp -rf ./opencc ~/.config/ibus/rime
-
-mkdir -p ~/.config/ibus/rime/build
-
 cp -rf ./ibus_rime.yaml ~/.config/ibus/rime/build
-
-mkdir -p ~/.config/fcitx/rime
-
 cp -rf ./*.yaml ~/.config/fcitx/rime
 cp -rf ./opencc ~/.config/fcitx/rime
 
-chmod -R 755 /usr/share/rime-data
+sudo chmod -R 755 /usr/share/rime-data
 
-chmod -R 777 ~/.config/ibus
+sudo chmod -R 777 ~/.config/ibus
 
-chmod -R 777 ~/.config/fcitx
+sudo chmod -R 777 ~/.config/fcitx
 
-rm -rf /usr/share/fonts/98WB
+echo "输入法相关文件安置结束，为码元拆分提示准备矢量字体支持"
 
-mkdir -p /usr/share/fonts/98WB
+sudo rm -rf /usr/share/fonts/98WB
 
-cp -rf ./fonts/98WB-1.otf /usr/share/fonts/98WB
+sudo mkdir -p /usr/share/fonts/98WB
 
-chmod 755 /usr/share/fonts/98WB/98WB-1.otf
+sudo cp -rf ./fonts/98WB-1.otf /usr/share/fonts/98WB
+
+sudo chmod 755 /usr/share/fonts/98WB/98WB-1.otf
 
 cd /usr/share/fonts/98WB
 
-mkfontscale
+sudo mkfontscale
 
-mkfontdir 
+sudo mkfontdir 
 
-fc-cache -fv
+sudo fc-cache -fv
 
-chmod -R 755 /usr/share/fonts/98WB
+sudo chmod -R 755 /usr/share/fonts/98WB
+
+echo "矢量字体添加成功"
 
 echo "中州韻配置文件安置成功，注銷系統後將重新部署。"
